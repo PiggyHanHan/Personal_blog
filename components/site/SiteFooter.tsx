b40ac2f38592ea5c38a6a85fe9e53e5cbdcdec83
blog-dev <blog@local.dev>
2026-08-10 17:39:24 +0800

    页脚：版权与建站时间分两行显示

--- components/site/SiteFooter.tsx
+++ components/site/SiteFooter.tsx
@@ -1,10 +1,11 @@
 import { SITE } from "@/lib/site";
 
-// 页脚：金线上沿 + 暖棕小字
+// 页脚：金线上沿 + 版权行 + 建站时间
 export default function SiteFooter() {
   return (
     <footer className="site-footer">
       <p>{SITE.footer}</p>
+      <p className="site-footer__built">{SITE.builtAt}</p>
     </footer>
   );
 }
